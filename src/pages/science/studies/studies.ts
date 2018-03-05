5b7590d5938b43ca211e2716a140ab431745b649
chris-caidson <chris@caidson.com>
2018-03-05 17:10:59 -0500

    Implemented Science Studies page.

--- src/pages/science/studies/studies.ts
+++ src/pages/science/studies/studies.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AuthProvider } from "../../../providers/auth/auth";
+import { PdfProvider } from "../../../providers/pdf/pdf";
 
 @IonicPage()
 @Component({
@@ -12,6 +13,7 @@ export class StudiesPage {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
+    public pdfProvider: PdfProvider,
     public auth: AuthProvider) {
   }
 
