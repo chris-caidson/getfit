577cc5bf01548cdbf6220b961c911fa9081e1a9e
chris-caidson <chris@caidson.com>
2018-03-08 13:59:32 -0500

    Implemented Friends and Family program page.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild, HostListener } from '@angular/core';
+import { IonicPage, NavController, NavParams, Slides, Platform } from 'ionic-angular';
 import { AuthProvider } from "../../providers/auth/auth";
 
 @IonicPage()
@@ -8,11 +8,26 @@ import { AuthProvider } from "../../providers/auth/auth";
   templateUrl: 'home.html',
 })
 export class HomePage {
+  spacer: number = 320;
+  year: number = (new Date()).getFullYear();
+  @ViewChild(Slides) slides: Slides;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public auth: AuthProvider) {
+    public auth: AuthProvider,
+    private platform: Platform) {
   }
 
+  ionViewDidLoad() {
+    this.platform.ready().then((readySource) => {
+      this.slides.slidesPerView =this.platform.width() / this.spacer;
+    });
+  }
+
+  onResize(event) {
+    const innerWidth = event.target.innerWidth;
+
+    this.slides.slidesPerView = innerWidth / this.spacer;
+ }
 }
--- src/pages/systems/friends-and-family-program/friends-and-family-program.ts
+++ src/pages/systems/friends-and-family-program/friends-and-family-program.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 import { AuthProvider } from "../../../providers/auth/auth";
 import { PdfProvider } from "../../../providers/pdf/pdf";
 
@@ -14,7 +14,18 @@ export class FriendsAndFamilyProgramPage {
     public navCtrl: NavController,
     public navParams: NavParams,
     public pdfProvider: PdfProvider,
+    public modalCtrl: ModalController,
     public auth: AuthProvider) {
+  }
+
+  openVideoModal() {
+    const myModal = this.modalCtrl.create("VideoModalPage", {
+      name: "You + 2, Them + 2",
+      url:
+        "https://firebasestorage.googleapis.com/v0/b/getfitglobal-bbef0.appspot.com/o/mp4s%2Fyou-plus-two-them-plus-two.mp4?alt=media&token=49d56d54-ba00-47fd-aa4b-1ac1fd1dadad"
+    });
+
+    myModal.present();
   }
 
 }
