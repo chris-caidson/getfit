19d036fbc24b93950f066593af534c346e84fa76
chris-caidson <chris@caidson.com>
2018-03-11 09:59:25 -0400

    Implemented IsaBody Challenge page.

--- src/pages/results/isabody-challenge/isabody-challenge.ts
+++ src/pages/results/isabody-challenge/isabody-challenge.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 import { AuthProvider } from "../../../providers/auth/auth";
+import { PdfProvider } from "../../../providers/pdf/pdf";
 
 @IonicPage()
 @Component({
@@ -12,6 +13,19 @@ export class IsabodyChallengePage {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
+    public modalCtrl: ModalController,
+    public pdfProvider: PdfProvider,
     public auth: AuthProvider) {
   }
+
+  openVideoModal() {
+    const myModal = this.modalCtrl.create("VideoModalPage", {
+      name: "The IsaBody Mission",
+      url:
+        "https://firebasestorage.googleapis.com/v0/b/getfreeglobal.appspot.com/o/videos%2Fisabody-mission.mp4?alt=media&token=911426dc-9c6f-4a51-b2ab-c29cba6200a6"
+    });
+
+    myModal.present();
+  }
+
 }
